afbac786b43d1f063fbde2decea3affc5f6b8926
unknown <abasian.arman@gmail.com>
2024-02-11 13:29:33 +0330

    some changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,34 @@
+import { Link } from "react-router-dom";
 import Stats from "../features/admin/Stats";
+import useUser from "../features/authentication/useUser";
 import ProjectListHome from "../features/projects/ProjectListHome";
 import { useGetAllProjects } from "../hooks/useProjects";
+import DarkModeToggle from "../ui/DarkModeToggle";
 import Loading from "../ui/Loading";
 
 function Home() {
   const { isLoading, projects = [] } = useGetAllProjects();
+  const { user, isLoading: isGettingUser } = useUser();
+  console.log(user);
   console.log(projects);
   return (
     <div className="h-screen p-2 bg-secondary-0 text-secondary-900 flex flex-col">
+      <div className="flex-none container xl:max-w-xl flex items-center justify-between">
+        {isGettingUser ? (
+          <Loading width={40} height={40} />
+        ) : (
+          <div>
+            {user ? (
+              <p className="text-primary-900">{user.name}</p>
+            ) : (
+              <Link to={"auth"} className="text-primary-900">
+                وارد شوید
+              </Link>
+            )}
+          </div>
+        )}
+        <DarkModeToggle />
+      </div>
       <h1 className="flex-none text-center pt-10 mb-10 text-2xl font-bold">
         پروژه‌های دلخواه خود را ببین, انتخاب کن و انجامش بده{" "}
       </h1>
